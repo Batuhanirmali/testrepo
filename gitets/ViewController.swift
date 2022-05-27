9b59af4daf6d6c71324560c7adba5aa726ec0b71
Batuhanirmali <batuqwert1@gmail.com>
2022-05-27 14:16:35 +0300

    Initial Commit

---
+++ gitets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitets
+//
+//  Created by Talha Batuhan Irmalı on 27.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
